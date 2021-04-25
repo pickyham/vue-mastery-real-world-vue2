5002998af3c4791128d1bdcc7bb49bad5d4e1616
Hyerim Yeom <ringkle777@gmail.com>
2021-04-26 00:19:38 +0900

    6. Single File Vue Components
    - Make an Event Card component

---
+++ real-world-vue/src/components/EventCard.vue
@@ -0,0 +1,36 @@
+<!-- body part -->
+<template>
+<!-- Must have 1 root element -->
+  <div>
+    <h4>{{title}}</h4>
+  </div>
+</template>
+
+<!-- brain -->
+<script>
+  export default {
+    // es6 shorthand
+    // also looks like this : 
+    //data : function(){...}
+    data(){
+      return{
+        title : 'Park Cleanup'
+      }
+    } 
+  }
+</script>
+
+<!-- clothes (dressed up) -->
+<style lang="scss" scoped>
+
+</style>
+
+<!-- each tags set property 'lang' 
+but there is a caveat. 
+there is a list to avoid it.
+1. Setup Preloaders
+2. Configure Webpack
+
+Check which dependencies are needed in the doc
+https://vue-loader.vuejs.org/guide/pre-processors.html
+-->
