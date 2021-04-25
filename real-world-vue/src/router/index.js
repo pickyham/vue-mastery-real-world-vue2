856d6c3f111c06c50e6818f2889716c339ecc4c0
Hyerim Yeom <ringkle777@gmail.com>
2021-04-25 14:56:39 +0900

    4.  Vue Router Basics
    - route alias&redirect
    - better to use 'redirect' for SEO than 'alias'. but good to know how to do it

--- real-world-vue/src/router/index.js
+++ real-world-vue/src/router/index.js
@@ -1,24 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
+    name: "home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/about-us",
+    name: "about",
+    component: About,
   },
+  // SEO 고려한다면 alias 보다 redirect가 더 좋을 듯
+  {
+    path : '/about',
+    redirect : {name : 'about'},
+  }
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  // },
 ];
 
 const router = new VueRouter({
